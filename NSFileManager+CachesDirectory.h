dda3072b5bf5d759eb0b0c0a2e40193ed0ea81f2
Matt Glover <matt@duchysoftware.com>
2013-01-15 09:32:26 +0000

    Inital commit - CachesDirectory & MoreColors

---
+++ NSFileManager+CachesDirectory.h
@@ -0,0 +1,13 @@
+//
+//  NSFileManager+CachesDirectory.h
+//  
+//
+//  Created by Matt Glover on 15/01/2013.
+//
+//
+
+@interface NSFileManager (CachesDirectory)
+
+- (NSString *)applicationCachesDirectory;
+
+@end
---
+++ NSFileManager+CachesDirectory.m
@@ -0,0 +1,27 @@
+//
+//  NSFileManager+CachesDirectory.m
+//  
+//
+//  Created by Matt Glover on 15/01/2013.
+//
+//
+
+#import "NSFileManager+CachesDirectory.h"
+
+@implementation NSFileManager (CachesDirectory)
+
+#pragma mark - Caches Directory Helper
+- (NSString *)applicationCachesDirectory {
+  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+  NSString *cachePath = [paths objectAtIndex:0];
+  BOOL isDir = NO;
+  NSError *error = nil;
+  
+  if (![[NSFileManager defaultManager] fileExistsAtPath:cachePath isDirectory:&isDir] && isDir == NO) {
+    [[NSFileManager defaultManager] createDirectoryAtPath:cachePath withIntermediateDirectories:NO attributes:nil error:&error];
+  }
+  
+  return cachePath;
+}
+
+@end
